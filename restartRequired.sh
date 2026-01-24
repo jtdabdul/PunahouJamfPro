7f3f4c7bc5bdae7b13680e1c087d3f36cf2e4dc7
jtdabdul <jtdabdul@gmail.com>
2026-01-23 14:42:34 -1000

    Update restartRequired.sh
    update comments to reflect code changes

--- restartRequired.sh
+++ restartRequired.sh
@@ -6,21 +6,20 @@
 
 # Static group - Restart Required to scope the policy running this script
 # Policy - Static Group remove Restart Required - remove the computer from the static group once reboot is executed, by the script or by the user
-# Policy - Clear Cache - remove computer from static group 689 (Restart Required), clear cache and restart, it will also clear the flag file
-# Policy - Punahou Branded prompt - prompt user allow deferral (button2), on restart (button1) call clearcache
+# Policy - Clear Cache - clear cache and restart
 
 # Script is designed to run in policy set to recurring check in ongoing, scoped to static group Restart Required
 # check for flag file existence and touch(create) if not found
 # check if user has rebooted on their own - if flag file age is > boot time interval - remove from static group and exit, preventing further execution
 # If computer has not yet rebooted, check to see if the threshold deadline is reached (days parameter passed in to script)
-# deadline not reached, call the branded prompt
+# deadline not reached, prompt user - button 2 (defer) do nothing and wait for next run.  button 1 (restart) clean up flag file and static group, then call clearcache
 # deadline reached - prompt restart imminent, call clearCache policy
 
 ##################################################################
 # To Do
 # 1. Clearcache policy could be more reusable if we decouple the static group remove and remove flag file steps - validate that these can be handled by callign the existing static group remove, then calling clearCache.  remove flag file may be redundant, as it is explicitly removed in the script
 # 2. Can the Branded prompt be implemented within this script, to eliminate an eternal dependency?
-#	if we do this, should we create a restart function which can be called from either the deadline or the user's choice at the prompt?
+#	if we do this, should we create a restart function which can be called from either the deadline or the user's choice at the prompt? - completed 2026/01/23
 # Try this after the current deployment is completed so that the reconfiguration can be tested
 
 
