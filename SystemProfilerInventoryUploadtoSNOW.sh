00da568d207bd16924b87dab5df2c225d28f354b
jtdabdul <jtdabdul@gmail.com>
2026-07-08 11:13:04 -1000

    Update SystemProfilerInventoryUploadtoSNOW.sh
    Add default header, put check interval into a function

--- SystemProfilerInventoryUploadtoSNOW.sh
+++ SystemProfilerInventoryUploadtoSNOW.sh
@@ -1,4 +1,68 @@
 #!/bin/bash
+########################################################################################################################
+# Default Header
+# Created by:	Jason Abdul (jabdul@punahou.edu)
+# Summary:		This is a default header intended to be included in shell scripts in jamf.  
+#				Commonly used Global variables, and functions are included.
+# Documentation: (#URL)
+#
+# Note:			This is a starter header, to be updated in the future.  
+#				Used Mann Consulting JNUC2025 header for reference. https://mann.com/jnuc2025
+# History:		Created on 2026/02/04 by JA
+########################################################################################################################
+### Global Variables
+VERSIONDATE='20260707'        # Format YYYYMMDD - used for version control
+APPLICATION="ApplicationName" # Change to your application name for logging
+LOCKED_THRESHOLD=604800       # If the computer is locked for this long (7 days) then exit to prevent issues with Jamf policies blocking other policies.
+SHUTDOWN_THRESHOLD=2592000    # If the computer is locked for this long (30 days) then prompt for shutdown.
+MINSWVERSION=13               # Minimum macOS version required to run this script
+#No Datadog implementation for our organization so far.
+DATADOGAPI=""                 # Add your Datadog API key here to enable Datadog logging, otherwise leave blank. NOTE: Mann recommends encryping your API keys.
+icon="/Library/Punahou/256x256PunahouSeal-transparent.png"	# Path to icon to use in jamfHelper windows  
+
+#MARK: Start Default Header
+##### Start Default Header 20260707
+PATH="/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin"
+scriptPath=${0}
+
+# Get current console user details safely in Bash
+consoleUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ { print $3 }')
+currentUser=${consoleUser:-UnknownUserName}
+
+currentUserID=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/kCGSSessionUserIDKey :/ { print $3 }')
+currentUserHome=$(dscl . -read Users/${currentUser} 2>/dev/null | grep ^NFSHomeDirectory | awk '{print $2}')
+jamfHelper="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
+LogDateFormat="%Y-%m-%d %H:%M:%S"
+starttime=$(date +"$LogDateFormat")
+LOCAL_LOG_FILE=""			# Add file path to enable local logging, otherwise leave blank
+
+
+### Start Logging 20250728
+readonly jamfVarJSSID=$(defaults read "/Library/Managed Preferences/com.mann.jamfuserdata.plist" JSSID 2>/dev/null || echo 0)
+readonly JSSURL=$(defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url 2>/dev/null) \
+hostname=${hostname:-$(hostname)} \
+computername=${computername:-$(scutil --get ComputerName 2>/dev/null)} \
+serialnumber=${serialnumber:-$(ioreg -d2 -c IOPlatformExpertDevice | awk -F\" '/IOPlatformSerialNumber/{print $(NF-1)}')} \
+SESSIONID=${jamfVarJSSID}-$RANDOM
+### Start logMessage 20260204
+logMessage() {
+	local message="$1"
+	if [[ "$LOCAL_LOG_FILE" == "" ]]; then
+		echo "$(date +"$LogDateFormat") - $message"
+	else
+		echo "$(date +"$LogDateFormat") - $message" | tee -a "$LOCAL_LOG_FILE"
+	fi
+}
+### End logMessage
+### Start runAsUser 20240419
+runAsUser() {
+	if [[ $currentUser != "loginwindow" ]]; then
+		uid=$(id -u "$currentUser")
+		launchctl asuser $uid sudo -u $currentUser "$@"
+	fi
+}
+### End runAsUser
+#MARK: End Default Header
 
 # Display NameDisplay name for the script
 # System Profiler Inventory Upload to SNOW launchagent and script
@@ -92,8 +156,7 @@ EOF
 	/bin/launchctl load -w $plistPath
 }
 function inventoryUploadToSNOW() {
-	echo ""
-	echo `date +%Y-%m-%d\ %H:%M:%S`
+	logMessage "Upload to SNow"
 	file="/Library/Punahou/applicationOutput.json"	#provide file path for applicationOutput.json
 	temp="/Library/Punahou/temp.json"	#provide file path for temp.json
 	# Get the serial number
@@ -110,12 +173,47 @@ function inventoryUploadToSNOW() {
 	sed "s/SPApplicationsDataType/records/g" $file > $temp
 	mv $temp $file
 	#push file to SNOW
-	curl "https://punahou.service-now.com/api/now/import/x_punsc_device_dat_computer_software_instance_import_set/insertMultiple" \
+	logMessage $(curl "https://punahou.service-now.com/api/now/import/x_punsc_device_dat_computer_software_instance_import_set/insertMultiple" \
 	--request POST \
 	--header "Accept:application/json" \
 	--header "Content-Type:application/json" \
 	--data "@$file" \
-	--user 'device_collector_software':'3hQX8}[$],jq^4PWY}hURLBT1raoqw*%94;Fc^a%Sh}D]1ko}sfskua{Q2BS_%%cM9n]NdD?zH--BQJ@k-JZ8m3yrqm{ZN=^zmn$'
+	--user 'device_collector_software':'3hQX8}[$],jq^4PWY}hURLBT1raoqw*%94;Fc^a%Sh}D]1ko}sfskua{Q2BS_%%cM9n]NdD?zH--BQJ@k-JZ8m3yrqm{ZN=^zmn$' 2>> $LOCAL_LOG_FILE)
+}
+check_interval() {
+	#For use if the launchdaemon is set to run more often then execution is desired.  Pass in an interval in seconds, and if the interval has passed since the last run (stored in a LAST_RUN_FILE, or LAST_RUN_FILE not found) then the function returns TRUE and updates LAST_RUN_FILE.  Otherwise it returns FASLE
+	#Usage: check_interval <interval in seconds> <tracking file path>
+	
+	local INTERVAL_SECONDS="$1"
+#	local LAST_RUN_FILE="$2"		using global LAST_RUN_FILE
+	local LAST_RUN_FILE="/Library/Punahou/InventorySNOW.last_run_time"
+	local CURRENT_TIME
+	local LAST_RUN_TIME
+	
+	# Get the current time in seconds since epoch
+	CURRENT_TIME=$(date +%s)
+	
+	# If the LAST_RUN_FILE doesn't exist, create it and return TRUE
+	if [ ! -f "$LAST_RUN_FILE" ]; then
+		echo "$CURRENT_TIME" > "$LAST_RUN_FILE"
+		return 0
+	fi
+	
+	#read the last run time
+	LAST_RUN_TIME=$(cat "$LAST_RUN_FILE")
+	
+	#calculate Time elapsed
+	TIME_DIFF=$((CURRENT_TIME - LAST_RUN_TIME))
+	
+	# check if interval has elapsed
+	if [ "$TIME_DIFF" -ge "$INTERVAL_SECONDS" ]; then
+		#update the LAST_RUN_FILE with the current time
+		echo "Time difference: $TIME_DIFF >$ threshold interval: $INTERVAL_SECONDS, return true"
+		return 0
+	else
+		echo "Threshold interval:$INTERVAL_SECONDS > Time difference: $TIME_DIFF, return false"
+		return 1
+	fi
 }
 ###################### MAIN #############################
 set_defaults
@@ -124,44 +222,12 @@ handle_plist
 # --- Your Actual Script Starts Here ---
 echo "Hello! I am running successfully from $INSTALL_PATH"
 
-
-logfile="/Library/Punahou/ApplicationsCheck.log"
-exec 1>> "$logfile"
-exec 2>&1
-
-###Template for launchDaemon to run script at shorter interval - execute the function only if 24 hours have passed since last run
-# File to store the last execution time
-LAST_RUN_FILE="/Library/Punahou/InventorySNOW.last_run_time"
-
-# Get the current time in seconds since epoch
-CURRENT_TIME=$(date +%s)
-
-# Check if the file exists
-if [[ -f "$LAST_RUN_FILE" ]]; then
-    # Read the last run time from the file
-    LAST_RUN_TIME=$(cat "$LAST_RUN_FILE")
+if check_interval 86400; then
+	echo "Interval passed, running the scheduled task"
+	inventoryUploadToSNOW 
 else
-    # If the file doesn't exist, set last run time to 0
-    LAST_RUN_TIME=0
+	logMessage "Too soon. Skipping task"
 fi
-
-# Calculate the time difference
-TIME_DIFF=$((CURRENT_TIME - LAST_RUN_TIME))
-
-# Check if more than 24 hours (86400 seconds) have passed
-if ((TIME_DIFF >= 86400)); then
-    #echo "Executing task..."
-    
-    # Place your task commands here
-    inventoryUploadToSNOW
-
-    # Update the last run time
-    echo "$CURRENT_TIME" > "$LAST_RUN_FILE"
-else
-	echo ""
-    echo "`date +%Y-%m-%d\ %H:%M:%S` Task was already executed within the last 24 hours."
-fi
-
 exit 0
 
 
